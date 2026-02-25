359a9725b1f0d7805700e57668559f2d9d2bf9cd
sourabhb2 <sourabhbhoyar85@gmail.com>
2026-02-25 22:08:02 +0530

    Test-2

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Sourabh! Python deployed successfully"
-
+    return "Hello Sourabh! Python deployed successfully!!!"
+    return "Hello"
 # IMPORTANT for Azure
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
